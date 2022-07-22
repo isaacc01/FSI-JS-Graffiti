34fb56644b9216523ed449e6ae22d735124e1868
Isaac Hernandez <Isaac@Isaacs-MacBook-Air.local>
2022-07-21 20:34:44 -0700

    my version of dogs

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
+
+let header = document.querySelector('#page-header')
+    header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+
+
+
+let footer = document.querySelector(".footer")
+footer.style.color = "red" ; 
+
+
+let dognames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.textAlign = 'left' ; 
+    dogImages[i].style.color = 'purple' ; 
+    dogImages[i].style.fontSize = '60px' ; 
+
+    
+
+}
+
+
+
 // Select some elements...
